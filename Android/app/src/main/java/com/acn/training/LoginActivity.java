af145ec6dbf690581b1c74c8ca3881b8e0da9d76
anthonius.setiawan <anthonius.setiawan@outlook.com>
2021-02-23 14:19:01 +0700

    initial repository

---
+++ Android/app/src/main/java/com/acn/training/LoginActivity.java
@@ -0,0 +1,106 @@
+package com.acn.training;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.os.Bundle;
+import androidx.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+//import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+	/** Called when the activity is first created. */
+	final Context context = this;
+	public static float INIT_BALANCE = 100;
+	private SharedPreferences userPreferences;
+	private EditText userNameEditField, passwordEditField;
+	private Button loginButton;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		try {
+			PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			pInfo.versionName = pInfo.versionName + "." + getVersion();
+			setTitle("Build Version : " + pInfo.versionName);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		userPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		userNameEditField = (EditText) findViewById(R.id.usernameTextField);
+		passwordEditField = (EditText) findViewById(R.id.passwordTextField);
+		loginButton = (Button) findViewById(R.id.loginButton);
+
+		loginButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				try {
+					String defaultUserName = "company";
+					String defaultPassword = "company";
+					if (userNameEditField.getEditableText().toString().trim().equalsIgnoreCase(defaultUserName) && passwordEditField.getEditableText().toString().trim().equalsIgnoreCase(defaultPassword)) {
+
+						Boolean isRefilled = userPreferences.getBoolean("Refilled", false);
+						if (!isRefilled) {
+							SharedPreferences.Editor prefsEditor = userPreferences.edit();
+							prefsEditor.putBoolean("Refilled", true);
+							prefsEditor.putFloat("Balance", INIT_BALANCE);
+							prefsEditor.commit();
+						}
+						finish();
+						startActivity(new Intent(LoginActivity.this, PaymentHomeActivity.class));
+					} else {
+						AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+						alertDialog.setTitle("Error").setMessage("Invalid username or password!");
+						alertDialog.setNeutralButton("Close", null).show();
+					}
+				} catch (Exception ex) {
+					Log.e(this.getClass().getName(), "Error : " + ex.getMessage(), ex);
+				}
+			}
+		});
+	}
+
+	public String getVersion() {
+		String valueToReturn = "Not valid";
+		String temp;
+		Properties buildNum = new Properties();
+		try {
+			InputStream rawResource = getResources().openRawResource(R.raw.version_number);
+			buildNum.load(rawResource);
+			temp = buildNum.getProperty("build.number");
+		} catch (Resources.NotFoundException e) {
+			System.err.println("Did not find raw resource: " + e);
+			return valueToReturn;
+		} catch (IOException e) {
+			System.err.println("Failed to open property file");
+			return valueToReturn;
+		} catch (Exception e) {
+			System.err.println("Failed to get 'build.number' property from version_number file.");
+			return valueToReturn;
+		}
+		if (temp != null) {
+			if (temp.length() > 0) {
+				valueToReturn = temp.toString().trim();
+			}
+		}
+		return valueToReturn;
+	}
+
+}
---
+++ Android/app/src/main/java/com/acn/training/ViewHolder.java
@@ -0,0 +1,9 @@
+package com.acn.training;
+
+import android.widget.TextView;
+
+public class ViewHolder {
+	public TextView title;
+	public TextView detail;
+	public TextView type;
+}
